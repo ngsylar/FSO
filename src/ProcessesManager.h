23d10bb90a8e9cdd1ce2a6d20de4a6a18d85f9e5
Caio Peluti <cpeluti@gmail.com>
2023-02-04 17:09:44 -0300

    interface do processmanager atualizada

--- src/ProcessesManager.h
+++ src/ProcessesManager.h
@@ -5,14 +5,19 @@
 #include<regex>
 #include<queue>
 #include "Process.h"
+#include "Operation.h"
 class ProcessManager {
     private:
         std::vector<std::vector<Process>> readyProcesses;
         void updateWaits();
         void checkWait();
         int max_wait;
-    public:
-        ProcessManager(int max_wait):readyProcesses(4){}
         void cycleQueues();
+    public:
+        // executa ciclo das filas
+        Operation run(){}
+        // max wait é o tempo de espera maximo de um processo antes dele aumentar a prioridade
+        ProcessManager(int max_wait):readyProcesses(4){}
+        // Adiciona processa em alguma das filas
         bool insertProcess(Process process);
 };
